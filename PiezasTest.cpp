42629e91c01c6e3317048256998cd21ff6658b7a
jsage1 <jsage1@mail.csuchico.edu>
2019-12-09 20:31:58 -0800

    "added corrosponding tests for insert"

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -26,3 +26,49 @@ TEST(PiezasTest, testingemptyboard)
 	Piece test = Blank;
 	ASSERT_EQ(got, test);
 }
+
+TEST(PiezasTest, getoutofbounds)
+{
+	Piezas game;
+	Piece got = game.pieceAt(5,5);
+	Piece test = Invalid;
+	ASSERT_EQ(got, test);
+}
+
+TEST(PiezasTest, testfirstplaced)
+{
+	Piezas game;
+	game.dropPiece(1);
+	Piece got = game.pieceAt(0,1);
+	Piece test = X;
+	ASSERT_EQ(got, test);
+}
+
+TEST(PiezasTest, testsecondplaced)
+{
+	Piezas game;
+	game.dropPiece(1);
+	game.dropPiece(1);
+	Piece got = game.pieceAt(1,1);
+	Piece test = O;
+	ASSERT_EQ(got, test);
+}
+
+TEST(PiezasTest, placingoutofbounds)
+{
+	Piezas game;	
+	Piece got = game.dropPiece(5);
+	Piece test = Invalid;
+	ASSERT_EQ(got, test);
+}
+
+TEST(PiezasTest, overplacing)
+{
+	Piezas game;
+	game.dropPiece(1);
+	game.dropPiece(1);
+	game.dropPiece(1);
+	Piece got = game.dropPiece(1);
+	Piece test = Blank;
+	ASSERT_EQ(got, test);
+}
